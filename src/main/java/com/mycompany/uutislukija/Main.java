d69e7fb45bc015971b920a1adbf712dc833589c4
TeemuKoivisto <teemu.koivisto@helsinki.fi>
2015-09-23 18:49:53 +0300

    alku

--- src/main/java/com/mycompany/uutislukija/Main.java
+++ src/main/java/com/mycompany/uutislukija/Main.java
@@ -2,17 +2,29 @@
 package com.mycompany.uutislukija;
 
 import com.mycompany.hackernewsuutiset.HackerPaivanUutiset;
+import static spark.Spark.*;
 
 public class Main {
     public static void main(String[] args){
-        HackerPaivanUutiset hakija = new HackerPaivanUutiset();
-        if(args.length == 0 || args[0].isEmpty()){
-            System.out.println("Et antanut komentoa. Käytettävissä olevat komennot: " + komennot());
-        } else if(args[0].equals("viimeisin")){
-            System.out.println(hakija.haeViimeisinUutinen());
-        } else if(args[0].equals("suosituin")){
-            System.out.println(hakija.haeSuosituinUutinen());
+//        port(getHerokuAssignedPort());
+//          get("hello", (req, res) -> "hello worldz");
+        
+//        HackerPaivanUutiset hakija = new HackerPaivanUutiset();
+//        if(args.length == 0 || args[0].isEmpty()){
+//            System.out.println("Et antanut komentoa. Käytettävissä olevat komennot: " + komennot());
+//        } else if(args[0].equals("viimeisin")){
+//            System.out.println(hakija.haeViimeisinUutinen());
+//        } else if(args[0].equals("suosituin")){
+//            System.out.println(hakija.haeSuosituinUutinen());
+//        }
+    }
+    
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
         }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
     }
     
     public static String komennot(){
