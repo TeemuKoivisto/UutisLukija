6355f5958a3a9527eac6450ab7b70927db2bafea
TeemuKoivisto <teemu.koivisto@helsinki.fi>
2015-09-24 16:39:56 +0300

    saatanan templatet nyt toimii.

--- src/main/java/com/mycompany/uutislukija/Main.java
+++ src/main/java/com/mycompany/uutislukija/Main.java
@@ -2,12 +2,41 @@
 package com.mycompany.uutislukija;
 
 import com.mycompany.hackernewsuutiset.HackerPaivanUutiset;
+import com.mycompany.uutislukija.domain.Uutinen;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import spark.ModelAndView;
+import spark.Spark;
+import spark.template.velocity.VelocityTemplateEngine;
+
 import static spark.Spark.*;
 
 public class Main {
     public static void main(String[] args){
 //        port(getHerokuAssignedPort());
-//          get("hello", (req, res) -> "hello worldz");
+//        Spark.staticFileLocation("/public");
+        get("/", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            model.put("uutinen", new Uutinen("homoilu yleistynyt", "localhost:4567/homo"));
+
+            return new ModelAndView(model, "public/templates/index.wm");
+        }, new VelocityTemplateEngine());
+        
+        get("/suosituin", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            model.put("uutinen", new Uutinen("homoilu yleistynyt", "localhost:4567/homo"));
+
+            return new ModelAndView(model, "public/templates/suosituin.wm");
+        }, new VelocityTemplateEngine());
+        
+        get("/viimeisin", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            model.put("uutinen", new Uutinen("homoilu yleistynyt", "localhost:4567/homo"));
+
+            return new ModelAndView(model, "public/templates/viimeisin.wm");
+        }, new VelocityTemplateEngine());
         
 //        HackerPaivanUutiset hakija = new HackerPaivanUutiset();
 //        if(args.length == 0 || args[0].isEmpty()){
@@ -19,13 +48,13 @@ public class Main {
 //        }
     }
     
-    static int getHerokuAssignedPort() {
-        ProcessBuilder processBuilder = new ProcessBuilder();
-        if (processBuilder.environment().get("PORT") != null) {
-            return Integer.parseInt(processBuilder.environment().get("PORT"));
-        }
-        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
-    }
+//    static int getHerokuAssignedPort() {
+//        ProcessBuilder processBuilder = new ProcessBuilder();
+//        if (processBuilder.environment().get("PORT") != null) {
+//            return Integer.parseInt(processBuilder.environment().get("PORT"));
+//        }
+//        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+//    }
     
     public static String komennot(){
         return "Komennot: viimeisin, suosituin";
