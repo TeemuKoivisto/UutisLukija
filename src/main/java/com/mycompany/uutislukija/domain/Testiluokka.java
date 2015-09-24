5fe8297a81762ae2c85de653a60d0e238a84cab4
TeemuKoivisto <teemu.koivisto@helsinki.fi>
2015-09-24 22:28:02 +0300

    testiluokka lisätty

---
+++ src/main/java/com/mycompany/uutislukija/domain/Testiluokka.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.uutislukija.domain;
+
+/**
+ *
+ * @author teemu
+ */
+public class Testiluokka {
+
+    public Testiluokka() {
+    }
+
+    public int testiMetodi() {
+        return 1;
+    }
+}
