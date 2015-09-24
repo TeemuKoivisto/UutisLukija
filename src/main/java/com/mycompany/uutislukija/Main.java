31a5561f6f60c3e18091b92cc49702c5d8ab661c
TeemuKoivisto <teemu.koivisto@helsinki.fi>
2015-09-24 17:26:24 +0300

    herokun paska toimii

--- src/main/java/com/mycompany/uutislukija/Main.java
+++ src/main/java/com/mycompany/uutislukija/Main.java
@@ -15,25 +15,34 @@ import static spark.Spark.*;
 
 public class Main {
     public static void main(String[] args){
-//        port(getHerokuAssignedPort());
-//        Spark.staticFileLocation("/public");
+        HackerPaivanUutiset hakija = new HackerPaivanUutiset();
+        port(getHerokuAssignedPort());
+        
         get("/", (req, res) -> {
-            Map<String, Object> model = new HashMap<>();
-            model.put("uutinen", new Uutinen("homoilu yleistynyt", "localhost:4567/homo"));
-
-            return new ModelAndView(model, "public/templates/index.wm");
+            return new spark.ModelAndView(new HashMap<>(), "public/templates/index.wm");
         }, new VelocityTemplateEngine());
         
+//        get("/", (req, res) -> {
+//            Map<String, Object> model = new HashMap<>();
+//            model.put("uutinen", new Uutinen("homoilu yleistynyt", "localhost:4567/homo"));
+//
+//            return new ModelAndView(model, "public/templates/index.wm");
+//        }, new VelocityTemplateEngine());
+        
         get("/suosituin", (req, res) -> {
+//            System.out.println();
+            
             Map<String, Object> model = new HashMap<>();
-            model.put("uutinen", new Uutinen("homoilu yleistynyt", "localhost:4567/homo"));
+            model.put("uutinen", new Uutinen("homoilu yleistynyt", hakija.haeSuosituinUutinen()));
 
             return new ModelAndView(model, "public/templates/suosituin.wm");
         }, new VelocityTemplateEngine());
         
         get("/viimeisin", (req, res) -> {
+//            System.out.println();
+            
             Map<String, Object> model = new HashMap<>();
-            model.put("uutinen", new Uutinen("homoilu yleistynyt", "localhost:4567/homo"));
+            model.put("uutinen", new Uutinen("homoilu yleistynyt", hakija.haeViimeisinUutinen()));
 
             return new ModelAndView(model, "public/templates/viimeisin.wm");
         }, new VelocityTemplateEngine());
@@ -48,13 +57,13 @@ public class Main {
 //        }
     }
     
-//    static int getHerokuAssignedPort() {
-//        ProcessBuilder processBuilder = new ProcessBuilder();
-//        if (processBuilder.environment().get("PORT") != null) {
-//            return Integer.parseInt(processBuilder.environment().get("PORT"));
-//        }
-//        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
-//    }
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
     
     public static String komennot(){
         return "Komennot: viimeisin, suosituin";
